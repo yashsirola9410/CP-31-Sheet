ea21553a7c7e6f67345f897162b3472b8cd56827
yashsirola9410 <yashtechid9410@gmail.com>
2026-06-06 22:03:33 +0530

    Bpermutation

--- BPermutationSwap.java
+++ BPermutationSwap.java
@@ -1,7 +1,31 @@
 import java.util.*;
 
 public class BPermutationSwap{
+      static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
     public static void main(String args[]){
-        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while(t-- > 0){
+            int n = sc.nextInt();
+
+            int ans = 0;
+            for(int i = 1 ; i <= n ;i++){
+                int x = sc.nextInt();
+                ans = gcd(ans , Math.abs(x - i));
+            }
+
+            System.out.println(ans);
+        }
+        sc.close();
+
     }
 }
